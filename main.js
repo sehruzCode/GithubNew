5a940e750bdce5416e3829306ace6124361bfd17
sehruzCode <shehruz98@mail.ru>
2024-08-29 18:26:05 +0400

    VErsion

---
+++ main.js
@@ -0,0 +1,59 @@
+let array = [1,2,3,4,5]
+
+
+
+const user1 = {
+    name: "Mehemmed",
+    surname: "Sahverdiyev",
+    age: 22,
+    hobbies: ["kitab", "musiqi"],
+    changeName(){
+        console.log("a");
+        
+    }
+
+}
+
+
+let newObj = {
+    ...user1,
+    name: "Omer",
+    gender: "Mann"
+}
+
+
+
+console.log(newObj);
+
+
+
+
+
+
+
+
+
+
+
+let user2 = {
+    name: "Sebine",
+    surname: "Dadasova",
+    age: 22,
+    hobbies: ["film", "gezmey"]
+}
+
+let user3 = {
+    name: "Ayten",
+    surname: "Qurbanli",
+    age: 19,
+    hobbies: ["kitab", "musiqi"]
+}
+
+
+
+
+
+
+
+
+
